5937a1ddcede7a4b74a27d9e03b58bacf6bcc82c
Wen-Kai Shen <wkai96815@gmail.com>
2015-07-05 17:44:53 -0500

    Initial Commit

---
+++ GitTest/src/com/vogella/git/first/GitTest.java
@@ -0,0 +1,14 @@
+package com.vogella.git.first;
+
+public class GitTest {
+
+	public GitTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Git is fun");
+	}
+
+}
